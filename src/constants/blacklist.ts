8481bc005cc572a5d873cfa4affc95ddb237109e
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-03-19 21:28:42 -0300

    Add blacklist of domains to filter search results
    - Introduced a new blacklist.ts file containing a list of domains to be excluded from search results.
    - This addition aims to enhance the relevance of search outcomes by filtering out unwanted sources.

---
+++ src/constants/blacklist.ts
@@ -0,0 +1,11 @@
+// Blacklist of domains to filter out from search results
+export const BLACKLISTED_DOMAINS = [
+  'tiktok.com',
+  'youtube.com',
+  'indeed.com',
+  'glassdoor.com',
+  'twitter.com',
+  'serasaexperian.com.br',
+  'cnpj.biz',
+  'econodata.com.br',
+];
